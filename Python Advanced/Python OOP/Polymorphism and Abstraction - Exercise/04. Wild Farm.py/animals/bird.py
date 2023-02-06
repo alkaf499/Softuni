207776efa99c6c2311d093860245f26dd26a4bc4
Arktic*-* <30373072+alkaf499@users.noreply.github.com>
2023-02-06 14:01:16 +0200

    Create bird.py

---
+++ Python Advanced/Python OOP/Polymorphism and Abstraction - Exercise/04. Wild Farm.py/animals/bird.py
@@ -0,0 +1,36 @@
+from project.animals.animal import Bird
+from project.food import Vegetable, Fruit, Seed, Meat
+
+
+class Owl(Bird):
+
+    def __init__(self, name, weight, wing_size: float):
+        super().__init__(name, weight, wing_size)
+
+    @property
+    def food_that_eats(self):
+        return [Meat]
+
+    @property
+    def gained_weight(self):
+        return 0.25
+
+    def make_sound(self):
+        return "Hoot Hoot"
+
+
+class Hen(Bird):
+    def __init__(self, name, weight, wing_size):
+        super().__init__(name, weight, wing_size)
+
+    @property
+    def food_that_eats(self):
+        return [Vegetable, Fruit, Seed, Meat]
+
+    @property
+    def gained_weight(self):
+        return 0.35
+
+    def make_sound(self):
+        return "Cluck"
+
