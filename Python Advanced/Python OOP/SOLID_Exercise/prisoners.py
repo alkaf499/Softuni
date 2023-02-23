bd4b3fbd70e8a8b0f977536242e47c74be008b65
Arktic*-* <30373072+alkaf499@users.noreply.github.com>
2023-02-23 12:47:51 +0200

    Create prisoners.py

---
+++ Python Advanced/Python OOP/SOLID_Exercise/prisoners.py
@@ -0,0 +1,46 @@
+import copy
+
+class Person:
+    def __init__(self, position):
+        self.position = position
+
+
+class Walk:
+
+    def walk_north(self, dist):
+        self.position[1] += dist
+
+    def walk_east(self, dist):
+        self.position[0] += dist
+
+    def walk_west(self, dist):
+        ...
+    def walk_south(self, dist):
+        ...
+
+    
+class FreePerson(Person, Walk):
+
+    def __init__(self, position):
+        super().__init__(position)
+
+
+class Prisoner(Person):
+    PRISON_LOCATION = [3, 3]
+
+    def __init__(self):
+        super(Prisoner, self).__init__(copy.copy(self.PRISON_LOCATION))
+        self.is_free = False
+
+
+prisoner = Prisoner()
+print("The prisoner trying to walk to north by 10 and east by -3.")
+
+try:
+    prisoner.walk_north(10)
+    prisoner.walk_east(-3)
+except:
+    pass
+
+print(f"The location of the prison: {prisoner.PRISON_LOCATION}")
+print(f"The current position of the prisoner: {prisoner.position}")
